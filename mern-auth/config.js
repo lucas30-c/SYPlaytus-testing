9627862de72600a754924086db43c25b912620dd
lucas30-c <96409387+lucas30-c@users.noreply.github.com>
2023-08-17 16:35:32 +1000

    Delete mern-auth directory

--- mern-auth/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGO_URI: 'mongodb+srv://zhenguo:863siWk53uvyDuTP@cluster0.hkmcspj.mongodb.net/userdb?retryWrites=true&w=majority',
-    //JWT_SECRET: 'YOUR_RANDOM_SECRET_FOR_JWT'  // If you're using JWT for authentication, you'll need this secret key
-};
